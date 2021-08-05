bac807fa4bd85b0c66519b34c5db7d4acab66821
Saber Alipour <alipouor@gmail.com>
2021-08-05 12:02:17 +0430

    Updates

---
+++ precache-manifest.ca40eafa9e1de2f49132850074522c39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d642a006c5d0a5426b482032647fdb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e86251a83d8498fba301",
+    "url": "./static/css/main.cd1ebdd8.chunk.css"
+  },
+  {
+    "revision": "6398fbec4412a2a9eb55",
+    "url": "./static/js/2.d02b2822.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d02b2822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86251a83d8498fba301",
+    "url": "./static/js/main.5c444f57.chunk.js"
+  },
+  {
+    "revision": "4cb5423c176c7ec38c8a",
+    "url": "./static/js/runtime-main.eaa90630.js"
+  },
+  {
+    "revision": "ced2fe27459062a55298c80f5afbcee6",
+    "url": "./static/media/after.ced2fe27.jpg"
+  },
+  {
+    "revision": "7acabd0e86ff5a923969622477d91b02",
+    "url": "./static/media/before.7acabd0e.jpg"
+  }
+]);
